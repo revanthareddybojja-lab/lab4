eb45b811094a16214bbb615fc1b92e45fd1c7179
REVANTH <121494570+LUCIFLASH122@users.noreply.github.com>
2026-01-23 11:35:29 +0530

    lab-4

--- train.py
+++ train.py
@@ -1,23 +1,36 @@
-import json
+import pandas as pd
 import joblib
-import numpy as np
+import json
 import os
 from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
 
-X = np.array([[1], [2], [3], [4]])
-y = np.array([0, 0, 1, 1])
+# Load dataset
+df = pd.read_csv("dataset/winequality-red.csv", sep=";")
 
-model = LogisticRegression()
-model.fit(X, y)
+X = df.drop("quality", axis=1)   # 11 features
+y = df["quality"]
 
-preds = model.predict(X)
-acc = accuracy_score(y, preds)
+# Train-test split
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, random_state=42
+)
 
+# Train model
+model = LogisticRegression(max_iter=1000)
+model.fit(X_train, y_train)
+
+# Evaluate
+preds = model.predict(X_test)
+acc = accuracy_score(y_test, preds)
+
+# Save model
 os.makedirs("models", exist_ok=True)
 joblib.dump(model, "models/model.pkl")
 
+# Save metrics
 with open("metrics.json", "w") as f:
     json.dump({"accuracy": acc}, f)
 
-print("Training done | Accuracy:", acc)
+print("Training completed. Accuracy:", acc)
